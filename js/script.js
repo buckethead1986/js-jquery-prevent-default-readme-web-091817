5d34d71adce23205fcabfeeddbc0df4917b83a8d
buckethead1986 <rspeckpfs@gmail.com>
2017-11-05 13:46:18 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
   // browser refreshes on submit
-  $('form').on('submit', function(){
-    var name = $('#name').val();
-    $("#hello").text("Hello, " + name);
-  });
-
-  //examine event object
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
+  // $("form").on("submit", function(ev) {
+  //   var name = $("#name").val();
+  //   $("#hello").text("Hello, " + name);
+  //   ev.preventDefault();
+  // });
+  // examine event object
+  // $("form").on("submit", function(event) {
+  //   var name = $("#name").val();
   //   $("#hello").text("Hello, " + name);
   //   debugger;
   //   event.preventDefault();
   // });
-
   // stop page refresh
-  // $('form').on('submit', function(event){
-  //   var name = $('#name').val();
-  //   $("#hello").text("Hello, " + name);
-  //   event.preventDefault();
-  // });
-
+  $("form").on("submit", function(event) {
+    var name = $("#name").val();
+    $("#hello").text("Hello, " + name);
+    event.preventDefault();
+  });
 });
